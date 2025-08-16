3bf1a2aefbcd6c3d9edbacb12a704d120286dfdf
AdrianGriesel <600678@student.belgiumcampus.ac.za>
2025-08-16 14:57:51 +0200

    added parsing of objective function

--- LPR381ProjectGS2/LPInputParser.cs
+++ LPR381ProjectGS2/LPInputParser.cs
@@ -72,7 +72,55 @@ namespace LinearProgrammingSolver
                 throw new ArgumentException($"Error parsing input file: {ex.Message}", ex);
             }
         }
+        private static void ParseObjectiveFunction(string line, LPModel model)
+        {
+            var vars = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);//spilits the values
 
+            if (vars.Length < 3 || vars.Length % 2 == 0)
+                throw new ArgumentException("Invalid objective function format. Expected: max/min +/- number +/- number ...");
+
+            // Parse max/min
+            string problemType = vars[0].ToLower();
+            if (problemType == "max")
+                model.IsMaximization = true;
+            else if (problemType == "min")
+                model.IsMaximization = false;
+            else
+                throw new ArgumentException($"Invalid problem type: {problemType}. Must be 'max' or 'min'");
+
+            // Parse operator-coefficient pairs
+            for (int i = 1; i < vars.Length; i += 2)
+            {
+                if (i + 1 >= vars.Length)
+                    throw new ArgumentException("Missing coefficient after operator");
+
+                string operatorVar = vars[i];
+                string numberVar = vars[i + 1];
+
+                // Validate operator
+                if (operatorVar != "+" && operatorVar != "-")
+                    throw new ArgumentException($"Invalid operator: {operatorVar}. Must be '+' or '-'");
+
+                // Parse coefficient
+                if (double.TryParse(numberVar, out double coefficient))
+                {
+                    // Apply sign from operator
+                    if (operatorVar == "-")
+                        coefficient = -coefficient;
+
+                    model.ObjectiveCoefficients.Add(coefficient);
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid coefficient: {numberVar}");
+                }
+            }
+
+            model.NumberOfVariables = model.ObjectiveCoefficients.Count;
+
+            if (model.NumberOfVariables == 0)
+                throw new ArgumentException("No variables found in objective function");
+        }
 
     }
 }
