95e6d012fe95b9a979097cf81187911dc53aa3f6
AdrianGriesel <600678@student.belgiumcampus.ac.za>
2025-08-16 14:23:27 +0200

    added types

---
+++ LPR381ProjectGS2/LPInputParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LinearProgrammingSolver
+{
+    public class LPR381ProjectGS2
+    {
+        public enum ConstraintType
+        {
+            LessOrEqual,    // <=
+            GreaterOrEqual, // >=
+            Equal           // =
+        }
+        public enum VariableType
+        {
+            Positive,   // +
+            Negative,   // -
+            Unrestricted, // urs
+            Integer,    // int
+            Binary      // bin
+        }
+    }
+}
