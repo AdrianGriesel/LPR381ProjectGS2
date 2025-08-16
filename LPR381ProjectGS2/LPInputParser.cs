ca77e3d3704c74b80861b5bfc20db98a54af4f9a
AdrianGriesel <600678@student.belgiumcampus.ac.za>
2025-08-16 14:37:52 +0200

    moved it over and add get;set better to habe it in LPInputParser

--- LPR381ProjectGS2/LPInputParser.cs
+++ LPR381ProjectGS2/LPInputParser.cs
@@ -21,12 +21,26 @@ namespace LinearProgrammingSolver
             Integer,    // int
             Binary      // bin
         }
+        public class LPModel
+        {
+            public bool IsMaximization { get; set; }
+            public List<double> ObjectiveCoefficients { get; set; } = new List<double>();
+            public List<Constraint> Constraints { get; set; } = new List<Constraint>();
+            public List<VariableType> SignRestrictions { get; set; } = new List<VariableType>();
+            public int NumberOfVariables { get; set; }
+        }
 
-        
+        public class Constraint
+        {
+            public List<double> Coefficients { get; set; } = new List<double>();
+            public ConstraintType Type { get; set; }
+            public double RightHandSide { get; set; }
+        }
+
 
         public static LPModel ParseInputFile(string filePath)
         {
-            if(!File.Exists(filePath))//check if file does not exists then ,then give exception
+            if (!File.Exists(filePath))//check if file does not exists then ,then give exception
                 throw new FileNotFoundException($"Input file not found: {filePath}");
             string[] lines = File.ReadAllLines(filePath);//reads all line int an array
             if (lines.Length < 2)//to small give error
--- LPR381ProjectGS2/LPModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class LPModel
-{
-    public bool IsMaximization { get; set; }
-}
-
-
