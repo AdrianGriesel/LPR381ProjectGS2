62efc11bee0a5ffc444237d243c06ee53b0eb3d5
FrederikWKnoetze <frederikwknoetze@gmail.com>
2025-08-29 17:04:26 +0200

    trying

--- LPR381ProjectGS2/frmBranchAndBound.Designer.cs
+++ LPR381ProjectGS2/frmBranchAndBound.Designer.cs
@@ -28,12 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.tabPivots = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.gridSolution = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeNodes = new System.Windows.Forms.TreeView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tabPivots.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(286, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(116, 55);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "Load Model";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // tabPivots
+            // 
+            this.tabPivots.Controls.Add(this.tabPage1);
+            this.tabPivots.Controls.Add(this.tabPage2);
+            this.tabPivots.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPivots.Location = new System.Drawing.Point(0, 0);
+            this.tabPivots.Name = "tabPivots";
+            this.tabPivots.SelectedIndex = 0;
+            this.tabPivots.Size = new System.Drawing.Size(411, 186);
+            this.tabPivots.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(403, 160);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // gridSolution
+            // 
+            this.gridSolution.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridSolution.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridSolution.Location = new System.Drawing.Point(0, 0);
+            this.gridSolution.Name = "gridSolution";
+            this.gridSolution.Size = new System.Drawing.Size(411, 133);
+            this.gridSolution.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(87, 98);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeNodes);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(622, 323);
+            this.splitContainer1.SplitterDistance = 207;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // treeNodes
+            // 
+            this.treeNodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeNodes.Location = new System.Drawing.Point(0, 0);
+            this.treeNodes.Name = "treeNodes";
+            this.treeNodes.Size = new System.Drawing.Size(207, 323);
+            this.treeNodes.TabIndex = 0;
+            this.treeNodes.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeNodes_AfterSelect);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.gridSolution);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tabPivots);
+            this.splitContainer2.Size = new System.Drawing.Size(411, 323);
+            this.splitContainer2.SplitterDistance = 133;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // frmBranchAndBound
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnLoadFile);
+            this.Name = "frmBranchAndBound";
             this.Text = "frmBranchAndBound";
+            this.tabPivots.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridSolution)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.TabControl tabPivots;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView gridSolution;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeNodes;
+        private System.Windows.Forms.SplitContainer splitContainer2;
     }
 }
--- LPR381ProjectGS2/frmBranchAndBound.cs
+++ LPR381ProjectGS2/frmBranchAndBound.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinearProgrammingSolver;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LPR381ProjectGS2
 {
@@ -16,5 +18,80 @@ namespace LPR381ProjectGS2
         {
             InitializeComponent();
         }
+        private BranchAndBoundSolver solver;
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var model = LPInputParser.ParseInputFile(ofd.FileName);
+                        solver = new BranchAndBoundSolver(model);
+                        solver.Solve();
+                        PopulateTree();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}", "Parsing/Solver Error");
+                    }
+                }
+            }
+        }
+        private void PopulateTree()
+        {
+            treeNodes.Nodes.Clear();
+            foreach (var node in solver.AllNodes)
+            {
+                var tn = new TreeNode($"Node {node.Id} | Bound={node.Bound:F2}");
+                tn.Tag = node;
+                treeNodes.Nodes.Add(tn);
+            }
+        }
+
+        private void treeNodes_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+            if (e.Node?.Tag is Node node)
+            {
+                // Show solution variables
+                var dtVars = new DataTable();
+                dtVars.Columns.Add("Variable");
+                dtVars.Columns.Add("Value");
+                for (int i = 0; i < node.VariableAssignments.Length; i++)
+                    dtVars.Rows.Add($"x{i + 1}", node.VariableAssignments[i]);
+                gridSolution.DataSource = dtVars;
+
+                // Show pivot iterations
+                tabPivots.TabPages.Clear();
+                int iter = 1;
+                foreach (var tableau in node.Tableaus)
+                {
+                    var tab = new TabPage($"Pivot {iter++}");
+                    var grid = new DataGridView
+                    {
+                        Dock = DockStyle.Fill,
+                        ReadOnly = true,
+                        AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+                    };
+                    var dt = new DataTable();
+                    for (int c = 0; c < tableau.GetLength(1); c++)
+                        dt.Columns.Add($"C{c}");
+                    for (int r = 0; r < tableau.GetLength(0); r++)
+                    {
+                        var row = dt.NewRow();
+                        for (int c = 0; c < tableau.GetLength(1); c++)
+                            row[c] = tableau[r, c];
+                        dt.Rows.Add(row);
+                    }
+                    grid.DataSource = dt;
+                    tab.Controls.Add(grid);
+                    tabPivots.TabPages.Add(tab);
+                }
+            }
+        }
     }
 }
+
